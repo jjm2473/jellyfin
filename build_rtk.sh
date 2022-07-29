ef571985a9d8da7d222cdabd04de203b578c0483
jjm2473 <1129525450@qq.com>
2022-07-29 19:00:12 +0800

    update rtk build script

--- build_rtk.sh
+++ build_rtk.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-VERSION=v10.8-1
+VERSION=4.9-v10.8
 
-[ -f realtek/Dockerfile ] || ( mkdir realtek && curl -L https://github.com/jjm2473/rtd1296_prebuilt_target/releases/download/v1.0/jellyfin-rtk-docker_10-7.tar | tar -x -C realtek ) || exit 1
+# [ -f realtek/Dockerfile ] || ( mkdir realtek && curl -L https://github.com/jjm2473/rtd1296_prebuilt_target/releases/download/v1.0/jellyfin-rtk-docker_10-7_4-9.tar | tar -x -C realtek ) || exit 1
 
-docker build -t jjm2473/jellyfin-server-rtk:$VERSION -f deployment/Dockerfile.docker.arm64 . || exit 1
+# docker build -t jjm2473/jellyfin-server-rtk:$VERSION -f deployment/Dockerfile.docker.arm64 . || exit 1
 
-docker build -t jjm2473/jellyfin-rtk:$VERSION -f Dockerfile.rtk . || exit 1
+# [ -d realtek/jellyfin-web ] || cp -a /Volumes/data/src/jellyfin-web/dist realtek/jellyfin-web
+# docker build --cache-from jjm2473/jellyfin-rtk:$VERSION -t jjm2473/jellyfin-rtk-cache:$VERSION --build-arg VERSION=$VERSION -f Dockerfile.rtk-cache realtek || exit 1
+# docker tag jjm2473/jellyfin-rtk-cache:$VERSION localhost:5000/jjm2473/jellyfin-rtk-cache:$VERSION
+# docker push localhost:5000/jjm2473/jellyfin-rtk-cache:$VERSION
+
+docker build --pull -t jjm2473/jellyfin-rtk:$VERSION --build-arg VERSION=$VERSION -f Dockerfile.rtk realtek || exit 1
+
